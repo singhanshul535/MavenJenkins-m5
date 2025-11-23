00900e43b9506f4ad59043bb4d39eb6e72be790f
LENOVO <LENOVO@Anshi>
2025-11-23 17:05:55 +0530

    Created two new classes-class1&class2

---
+++ MavenJenkins-m5/src/test/java/class1/Class2.java
@@ -0,0 +1,10 @@
+package class1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hey from class 2");
+
+	}
+
+}
---
+++ MavenJenkins-m5/src/test/java/class2/Class1.java
@@ -0,0 +1,11 @@
+package class2;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hey");
+
+	}
+
+}
