1d2153b61aa484eafcb5ef6d5c3d186a5080664b
LENOVO <LENOVO@Anshi>
2025-11-26 12:48:30 +0530

    getting the contacts tab

---
+++ MavenJenkins-m5/src/test/java/vtigerContacts/GettingStarted.java
@@ -0,0 +1,33 @@
+package vtigerContacts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GettingStarted {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("http://localhost:8888/index.php?action=Login&module=Users");
+		
+		
+		WebElement user=driver.findElement(By.xpath("//input[@name='user_name']"));
+		user.sendKeys("admin");
+		
+		WebElement pswd=driver.findElement(By.xpath("//input[@name='user_password']"));
+		pswd.sendKeys("manager");
+		
+		WebElement login=driver.findElement(By.id("submitButton"));
+		login.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement ctab=driver.findElement(By.linkText("Contacts"));
+		ctab.click();
+		
+	}
+}
